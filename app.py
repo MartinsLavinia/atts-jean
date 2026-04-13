e2c6cfb213ef1cda4528fa2b2d55ea9455125e34
lavinia <mlavinia088@gmail.com>
2026-04-13 12:09:30 -0300

    feat: importação do flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+@app.route("/quem_somos")
+def quem_somos():
+    return render_template("quem_somos.html")
